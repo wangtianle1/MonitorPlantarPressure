e58424e8b15f128ffc56ef28e054d8b985be0e5c
wangtianle1 <2972865937@qq.com>
2023-08-22 02:09:32 +0800

    feat:fragment进行图表适配

--- app/src/main/java/com/example/monitorplantarpressure/MainFragment.java
+++ app/src/main/java/com/example/monitorplantarpressure/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.monitorplantarpressure;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +11,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainFragment extends Fragment {
     private static final String SECTION_STRING = "fragment_string";
-    public static MainFragment newInstance(String sectionNumber){
+
+    public static MainFragment newInstance(String sectionNumber) {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
         args.putString(SECTION_STRING, sectionNumber);
@@ -20,36 +33,36 @@ public class MainFragment extends Fragment {
         return fragment;
 
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.user_fragment, container, false);
 //        TextView textView = (TextView) view.findViewById(R.id.index_bottom_bar_fragment_example);
 //        textView.setText(getString(R.string.fragment_example_string, getArguments().getString(SECTION_STRING)));
+        LineChart lineChart = view.findViewById(R.id.lineChart);
+        // Add more entries as needed
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, 10));
+        entries.add(new Entry(2, 20));
+        entries.add(new Entry(3, 15));
+        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        dataSet.setColor(Color.BLUE);
+        dataSet.setValueTextColor(Color.BLACK   );
+
+        LineData lineData = new LineData(dataSet);
+        lineChart.setData(lineData);
+
+        // Customize chart settings
+        XAxis xAxis = lineChart.getXAxis();
+        YAxis yAxisLeft = lineChart.getAxisLeft();
+        YAxis yAxisRight = lineChart.getAxisRight();
+        Legend legend = lineChart.getLegend();
+
+        // Configure other settings as needed
+
+        lineChart.invalidate();
         return view;
     }
-//    LineChart lineChart = findViewById(R.id.lineChart);
 
-//        List<Entry> entries = new ArrayList<>();
-//        entries.add(new Entry(1, 10));
-//        entries.add(new Entry(2, 20));
-//        entries.add(new Entry(3, 15));
-    // Add more entries as needed
-
-//        LineDataSet dataSet = new LineDataSet(entries, "Label");
-//        dataSet.setColor(Color.BLUE);
-//        dataSet.setValueTextColor(Color.BLACK);
-//
-//        LineData lineData = new LineData(dataSet);
-//        lineChart.setData(lineData);
-
-    // Customize chart settings
-//        XAxis xAxis = lineChart.getXAxis();
-//        YAxis yAxisLeft = lineChart.getAxisLeft();
-//        YAxis yAxisRight = lineChart.getAxisRight();
-//        Legend legend = lineChart.getLegend();
-
-    // Configure other settings as needed
-
-//        lineChart.invalidate();
 }
