dbcbfbd682b2c201caa291270e05b246cd94d4fb
wangtianle1 <2972865937@qq.com>
2023-08-24 23:07:01 +0800

    feat:ChratFragment功能

--- app/src/main/java/com/example/monitorplantarpressure/ChartFragment.java
+++ app/src/main/java/com/example/monitorplantarpressure/ChartFragment.java
@@ -1,5 +1,6 @@
 package com.example.monitorplantarpressure;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +12,15 @@ import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.LineChart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChartFragment extends Fragment {
+    private DynamicLineChartManager dynamicLineChartManager1;
+    private DynamicLineChartManager dynamicLineChartManager2;
+    private List<Integer> list = new ArrayList<>(); //数据集合
+    private List<String> names = new ArrayList<>(); //折线名字集合
+    private List<Integer> colour = new ArrayList<>();//折线颜色集合
     public static final String SECTION_STRING = "chart";
     public static ChartFragment newInstance(String sectionNumber) {
         ChartFragment fragment = new ChartFragment();
@@ -28,16 +37,49 @@ public class ChartFragment extends Fragment {
         View view = inflater.inflate(R.layout.chart_layout,container,false);
         LineChart lineCharta = view.findViewById(R.id.lineCharta);
         LineChart lineChartb = view.findViewById(R.id.lineChartb);
-getActivity().runOnUiThread(new Runnable() {
-    @Override
-    public void run() {
+        names.add("温度");
+        names.add("压强");
+        names.add("其他");
+        //折线颜色
+        colour.add(Color.CYAN);
+        colour.add(Color.GREEN);
+        colour.add(Color.BLUE);
 
-    }
-});
+        dynamicLineChartManager1 = new DynamicLineChartManager(lineCharta, names.get(0), colour.get(0));
+        dynamicLineChartManager2 = new DynamicLineChartManager(lineChartb, names, colour);
+
+        dynamicLineChartManager1.setYAxis(100, 0, 10);
+        dynamicLineChartManager2.setYAxis(100, 0, 10);
+
+        //死循环添加数据
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+              getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            list.add((int) (Math.random() * 50) + 10);
+                            list.add((int) (Math.random() * 80) + 10);
+                            list.add((int) (Math.random() * 100));
+                            dynamicLineChartManager2.addEntry(list);
+                            list.clear();
+                        }
+                    });
+                }
+            }
+        }).start();
 
         return view;
 
 
     }
-
+    public void addEntry(View view) {
+        dynamicLineChartManager1.addEntry((int) (Math.random() * 100));
+    }
 }
